5ef650a98bb3e006bb0ff9cdb9f592a2124c09e4
HassanThePassionate <maqsoodia777@gmail.com>
2024-04-29 11:59:20 +0500

    Update

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -1,15 +1,40 @@
+"use client";
 import React from "react";
 import style from "./hero.module.css";
 import Tags from "./tags/Tags";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 const Hero = () => {
+  const [typeEffect] = useTypewriter({
+    words: [
+      "The Dark Knight",
+      "Superhero",
+      "Christan Bale",
+      "Toy Story 3",
+      "Friendship",
+      "Tom Hanks",
+      "Gladitors",
+      "Roman Empire",
+      "Sigourney Weaver",
+      "Russell Crowe",
+      "Alien",
+      "Spaceship",
+      "Sigourney Weaver",
+      "Joker",
+      "Mental Health",
+      "Joaquin Phoenix",
+    ],
+    loop: {},
+    typeSpeed: 200,
+    deleteSpeed: 100,
+  });
   return (
     <div className={style.hero}>
-      <div className={style.overlay}></div>
+      <div className={style.overlay}></div>;
       <div className={style.hero_wrap}>
         <h1 className={style.title}>
           Discover
           <span className={style.span_tag}>Similar</span>
-          <span className="block">
+          <span className='block'>
             Movies and TV
             <br />
             Shows!
@@ -18,12 +43,12 @@ const Hero = () => {
         <div className={style.search_hero}>
           <div className={style.search}>
             <form className={style.hero_form}>
-              <input type="text" />
+              <input type='text' />
               <button className={style.btn}>Explorer</button>
             </form>
-            <div id="hero_input_search" className={style.search_value}>
-              <span className="text-[24px] text-[#e8e8e8] font-bold">
-                Joker
+            <div id='hero_input_search' className={style.search_value}>
+              <span className='text-[24px] text-[#e8e8e8] font-bold'>
+                {typeEffect}
               </span>
             </div>
           </div>
---
+++ constant/Imgdata.js
@@ -0,0 +1,8 @@
+export const Imgdata = [
+  {
+    url: "https://similarwatch.com/_next/image/?url=https%3A%2F%2Fmovies.imgcdn.org%2Fmovies%2Falien-2019%2Falien-2019-cover-large.jpg&w=3840&q=75",
+  },
+  {
+    url: "https://similarwatch.com/_next/image/?url=https%3A%2F%2Fmovies.imgcdn.org%2Fmovies%2Fjoker-2019%2Fjoker-2019-cover-large.jpg&w=3840&q=75",
+  },
+];
